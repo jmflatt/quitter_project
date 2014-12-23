d2a2e01109afee0cc2ab4edfbde31deade304a56
jmflatt55 <jonathanm.flatt@gmail.com>
2014-12-23 13:29:38 +0000

    Finish user signup pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215185919) do
+ActiveRecord::Schema.define(version: 20141215192445) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password"
-    t.string   "digest"
     t.string   "password_digest"
   end
 
